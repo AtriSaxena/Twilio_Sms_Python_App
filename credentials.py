61646b3e978637a06f9d5ce2e29426c3f5c0700e
ATRI SAXENA <30728764+CoderAtri@users.noreply.github.com>
2017-10-26 22:20:26 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,5 @@
+#Credential Generated from your Twilio Account
+account_sid= 'Your Twilio Sid'
+auth_token='Your Twilio Auth Token'
+my_cell='Number whom you want to sms'
+my_twilio='Your Twilio number'
